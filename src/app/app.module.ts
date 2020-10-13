7cb22fd815010512e0cdae8db9011040d86e8f82
Misrraim <misrraimsp@gmail.com>
2020-10-13 18:09:26 +0100

    added proper currency and dates style with pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
